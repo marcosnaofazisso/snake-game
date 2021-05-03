94518692727945c90f3dacecc1f18bbdef68bbf2
marcosvmferreira <rm86904@fiap.com.br>
2021-05-03 18:55:22 -0300

    Adição da comida e atributos

--- snake.py
+++ snake.py
@@ -1,19 +1,39 @@
-import pygame
+import pygame, random
 from pygame.locals import *
+
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
 
 pygame.init()
 screen = pygame.display.set_mode((600,600))
 pygame.display.set_caption('Snake Game')
 
 snake = [(200, 200), (210, 200), (220, 200)]
+snake_skin = pygame.Surface((10, 10))
+snake_skin.fill((255, 255, 255))
+
+food = pygame.Surface((10, 10))
+food.fill((255, 0, 0))
+food_position = (random.randint(0,590), random.randint(0,590))
+
+
+snake_direction = LEFT
 
 
 while True:
-    for event pygame.event.get():
+    for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
 
-    pygame.display_update()
+    screen.fill((0, 0, 0))
+    screen.blit(food, food_position)
+    for position in snake:
+        screen.blit(snake_skin, position)
+
+    
+    pygame.display.update()
 
 
 
